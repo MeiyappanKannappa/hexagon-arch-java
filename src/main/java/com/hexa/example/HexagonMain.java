f2389615e2d7196f99fffc853f856a56c7efc562
Meiyappan <kmeiyap1@ford.com>
2018-11-21 11:54:11 +0530

    Code changes

--- src/main/java/com/hexa/example/HexagonMain.java
+++ src/main/java/com/hexa/example/HexagonMain.java
@@ -1,30 +1,32 @@
 package com.hexa.example;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.hexa.example.domain.FoodOrderSummary;
+import com.hexa.example.domain.FoodType;
+import com.hexa.example.factory.AdapterFactory;
+import com.hexa.example.ports.ReserveOrderPort;
 
 public class HexagonMain {
 
 	public static void main(String[] args) {
 		try {
-			Scanner scanner=new Scanner(System.in);
-			System.out.println("Enter The Item: ");
-			String item=scanner.nextLine();
-			System.out.println("This is item "+item);
-			/*ReserveOrderPort orderProcessor=AdapterFactory.getOrderProcessor("MOBILE");
-			ArrayList<FoodType> foodItems=new ArrayList<>();
-			String[] toppings= {"MUSHROOM","CHEESE","HAM"};
+
+			ReserveOrderPort orderProcessor = AdapterFactory.getOrderProcessor("MOBILE");
+			ArrayList<FoodType> foodItems = new ArrayList<>();
+			String[] toppings = { "MUSHROOM", "CHEESE", "HAM" };
 			foodItems.add(orderProcessor.createItem("PIZZA", Arrays.asList(toppings)));
-			
-			String[] burger_toppings= {"CHICKEN","MOZARELLA"};
+
+			String[] burger_toppings = { "CHICKEN", "CHEESE" };
 			foodItems.add(orderProcessor.createItem("BURGER", Arrays.asList(burger_toppings)));
-			FoodOrderSummary summary=orderProcessor.processOrder(foodItems);
+			FoodOrderSummary summary = orderProcessor.processOrder(foodItems);
 			System.out.println("**************************************************************");
-			System.out.println("*                   "+summary.toString()+"                   *");
-			System.out.println("**************************************************************");*/
+			System.out.println("*                   " + summary.toString() + "                   *");
+			System.out.println("**************************************************************");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 }
